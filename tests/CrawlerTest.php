e0afeb31bac7d52bb62305c24e5e112a0e32a27d
Herlandio <59981723+herlandio@users.noreply.github.com>
2022-05-10 12:23:19 -0300

    Create CrawlerTest.php

---
+++ tests/CrawlerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+require './src/crawler.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class CrawlerTest extends TestCase {
+
+    public function testArrayOfInfos(): void {
+        $instance = new WebCrawler();
+        $instance->getPages(5);
+        $this->assertIsArray($instance->getArrayOfInfos());
+    }
+
+    public function testQuantity(): void {
+        $instance = new WebCrawler();
+        $instance->getPages(4);
+        $this->assertEquals(150, count($instance->getArrayOfInfos()));
+    }
+
+}
