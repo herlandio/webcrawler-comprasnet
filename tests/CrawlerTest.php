ac8470df68e91f45b4d82f3c7ae67ba871493a45
Herlandio <59981723+herlandio@users.noreply.github.com>
2022-05-10 16:21:34 -0300

    Update CrawlerTest.php

--- tests/CrawlerTest.php
+++ tests/CrawlerTest.php
@@ -16,7 +16,7 @@ final class CrawlerTest extends TestCase {
 
     public function testQuantity(): void {
         $instance = new WebCrawler();
-        $instance->getPages(5);
+        $instance->getPages(4);
         $this->assertEquals(150, count($instance->getArrayOfInfos()));
     }
 
